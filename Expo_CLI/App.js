61125ece1856baf43582bc8a8e365529a5b654d2
dem0saic <vowusu032@st.ug.edu.gh>
2024-05-26 20:43:40 +0000

    Increase the font size of the text to 24.

--- Expo_CLI/App.js
+++ Expo_CLI/App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={textFont.container}>
-      <Text style={styles.fonts}>My name is Owusu Vincent</Text>
+    <View style={styles.container}>
+      <Text style={textFont.fonts}>My name is Owusu Vincent</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +18,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const textFont = StyleSheet.create({
+  fonts: {
+    fontSize: 24,
+    //fontWeight: 'bold',
+    color: '#000000'
+  }
+})
